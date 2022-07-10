501fb99e9a34357eb047c0cbb0ad723b0f4f6066
davidkamere <davidkamere@live.com>
2022-07-10 18:10:58 +0300

    Deploy app to Heroku

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,8 +4,8 @@ import { Link } from "react-router-dom";
 function App() {
   return (
     <>
-
-      <div className="flex gap-10 h-screen flex-col justify-center items-center">
+      <div className='flex justify-center mt-20 px-8  font-bold text-2xl'>Select an option below:</div>
+      <div className="flex gap-10 m-32 flex-col justify-center items-center">
         <Link
           to="/customers"
           className="bg-white text-center hover:bg-gray-100 text-gray-800 w-36 font-semibold py-2 px-4 border border-gray-400 rounded shadow"
